0d3e96ec2d597a84c69fed72cb68bbfc560ac384
Yash Kothari <yashk8119@gmail.com>
2025-09-07 21:07:21 +0530

    minor issue fix

--- src/comp/index.js
+++ src/comp/index.js
@@ -1,8 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Main from "./Main/Main";
-import Navbar  from "./NavBar/Navbar";
-import Registration from "./common/registration";
-import Abstract from "./common/Abstract"
+import Navbar from "./NavBar/Navbar";
+import Registration from "./common/Registration";
+import Abstract from "./common/Abstract";
 import Committee from "./Committee/Committee";
-export { Main, Footer, Header,Navbar,Registration,Abstract,Committee };
+export { Main, Footer, Header, Navbar, Registration, Abstract, Committee };
